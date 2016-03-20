9594a9eb7b8ebc24bc99f60e5512dd68c03f3d9a
Andrew Scott <amscott@openmailbox.org>
2016-03-20 19:05:53 +0000

    adds inception example

---
+++ dreaming.rb
@@ -0,0 +1,18 @@
+def a
+  puts "Entering the first dream"
+  b
+  puts "Leaving the first dream"
+end
+
+def b
+  puts "Entering the second dream"
+  c
+  puts "Leaving the second dream"
+end
+
+def c
+  puts "Entering the third dream"
+  puts "Leaving the third dream"
+end
+
+a
